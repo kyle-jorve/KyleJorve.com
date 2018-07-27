13a4a175587251877959d050bf7efb68b48260b4
Kyle Jorve <kylej@simplybits.com>
2018-07-26 22:30:19 -0700

    bug fixes, ux improvements, portfolio additions

--- KyleJorve.com/App_Data/Models/all.generated.cs
+++ KyleJorve.com/App_Data/Models/all.generated.cs
@@ -9,7 +9,7 @@ using  Umbraco.ModelsBuilder;
 using  Umbraco.ModelsBuilder.Umbraco;
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "c754287ade4ee1af")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 
 // FILE: models.generated.cs
